ba7630bdf1b8eac101e2da2746c1fb6745dda750
talking-tech <tarakbpatel@qucogroup.com>
2020-10-11 03:03:30 +0530

    Added Animation Fixed

--- assets/js/mysite.js
+++ assets/js/mysite.js
@@ -2,3 +2,16 @@ $(window).scroll(function(){
     $('nav').toggleClass('scrolled', $(this).scrollTop() > 500);
     $('nav').toggleClass('navbar-dark', $(this).scrollTop() > 500);
 });
+
+$(document).ready(function(){
+    setInterval(swapRoles,1000);
+    var masterroles = ["Student", "VITian", "Baller","Toastmaster","Reader","Web Developer"]; 
+    var count = 0;
+    function swapRoles(){
+        $('.roles').fadeOut("fast", function(){
+          $(this).text(masterroles[count++ % masterroles.length]);
+          $(this).fadeIn("fast");
+        });
+    };
+  });
+  
