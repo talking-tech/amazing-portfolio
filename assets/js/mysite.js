6c4b3be6d7bcb31325c6d5560f6dc38419428edd
talking-tech <72455894+talking-tech@users.noreply.github.com>
2020-10-11 14:26:52 +0530

    Update mysite.js

--- assets/js/mysite.js
+++ assets/js/mysite.js
@@ -1,9 +1,6 @@
-$(window).scroll(function(){
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 500);
-    $('nav').toggleClass('navbar-dark', $(this).scrollTop() > 500);
-});
-
 $(document).ready(function(){
+    
+    // Swapping Roles In Header
     setInterval(swapRoles,1000);
     var masterroles = ["Student", "VITian", "Baller","Toastmaster","Reader","Web Developer"]; 
     var count = 0;
@@ -13,5 +10,12 @@ $(document).ready(function(){
           $(this).fadeIn("fast");
         });
     };
+    
+    // Scrolling Navbar
+    $(window).scroll(function(){
+        $('nav').toggleClass('scrolled', $(this).scrollTop() > 500);
+        $('nav').toggleClass('navbar-dark', $(this).scrollTop() > 500);
+    });
+    
   });
   
